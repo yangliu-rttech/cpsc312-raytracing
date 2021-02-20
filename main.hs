ec50ed02049f7f3c51eff043dcc1bbe13eb40978
theo <theo.scofield@me.com>
2021-02-20 02:04:30 -0800

    add sphere math

--- main.hs
+++ main.hs
@@ -48,8 +48,11 @@ dot (x1,y1,z1) (x2,y2,z2) = x1*x2 + y1*y2 + z1*z2
 cross :: Vec -> Vec -> Vec
 cross (x1,y1,z1) (x2,y2,z2) = (y1*z2 - z1*y2, z1*x2-x1*z2,x1*y2-y1*x2)
 
+quadrance :: Vec -> Double 
+quadrance v = v `dot` v
+
 mag :: Vec -> Double
-mag v = sqrt (v `dot` v)
+mag v = sqrt (quadrance v)
 
 normalize :: Vec -> Vec 
 normalize v = v /. mag v
@@ -104,7 +107,7 @@ data Plane = Plane {pnt, nrm :: Vec}
 
 threePointsToPlane :: Vec -> Vec -> Vec -> Plane 
 threePointsToPlane a b c = Plane {pnt = (a +. b +. c) /. 3, nrm = norm}
-   where norm = (b -. a) `cross` (c -. a)
+   where norm = normalize ((b -. a) `cross` (c -. a))
 
 data Ray = Ray {pos, look :: Vec}
 
@@ -112,9 +115,35 @@ parallelTo :: Ray -> Plane -> Bool
 parallelTo ry pl = nrm pl `dot` look ry ~= 0
 
 --https://www.scratchapixel.com/lessons/3d-basic-rendering/minimal-ray-tracer-rendering-simple-shapes/ray-plane-and-ray-disk-intersection
+--returns maybe the r such that the point pos + r*look is on the plane
 planeIntersectDist :: Plane -> Ray -> Maybe Double 
 planeIntersectDist pl ry = if ry `parallelTo` pl then Nothing 
    else Just (((pnt pl -. pos ry) `dot` nrm pl) / (look ry `dot` nrm pl))
+
+--reflects the direction of a given vector off a given plane
+reflectDirOff :: Plane -> Vec -> Vec 
+reflectDirOff pl dir = 
+   let componentOfDirInDirectionOfNormal = nrm pl *. (nrm pl `dot` dir) in  
+   dir -. (componentOfDirInDirectionOfNormal *. 2)
+
+data Sphere = Sphere {radius :: Double, center :: Vec}
+
+sphereIntersectDist :: Sphere -> Ray -> Maybe Double
+sphereIntersectDist sph ry = 
+   let r2 = radius sph * radius sph in 
+   let center2pos = center sph -. pos ry in 
+   let distanceFromCenterToPosSquared = quadrance center2pos in 
+   let loc = center2pos `dot` look ry in 
+   let loc2 = loc * loc in 
+   let back2frontDistSquared = loc2 - distanceFromCenterToPosSquared + r2 in 
+   if back2frontDistSquared < 0 then Nothing else
+      let back2frontDist = sqrt back2frontDistSquared in 
+      let ans1 = -loc - back2frontDist in 
+      --ans1 is the distance to the front of the sphere and ans2 the distance to the back of the sphere
+      --if you're not inside the sphere, you want ans1
+      if ans1 > 0 then Just ans1 else 
+         let ans2 = -loc + back2frontDist in 
+         Just ans2
 
 data RayResult = NoHit |
    Hit {dist :: Float, color :: Vec}
