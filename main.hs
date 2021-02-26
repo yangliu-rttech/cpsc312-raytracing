364f1fa347c3f065cab14c3e8a2fa7d9d893c684
Yang Liu <lynch.liu@hotmail.com>
2021-02-25 23:17:24 -0800

    close to finish

--- main.hs
+++ main.hs
@@ -116,9 +116,10 @@ parallelTo ry pl = nrm pl `dot` look ry ~= 0
 
 --https://www.scratchapixel.com/lessons/3d-basic-rendering/minimal-ray-tracer-rendering-simple-shapes/ray-plane-and-ray-disk-intersection
 --returns maybe the r such that the point pos + r*look is on the plane
-planeIntersectDist :: Plane -> Ray -> Maybe Double 
+planeIntersectDist :: Plane -> Ray -> Maybe (Double,Vec) 
 planeIntersectDist pl ry = if ry `parallelTo` pl then Nothing 
-   else Just (((pnt pl -. pos ry) `dot` nrm pl) / (look ry `dot` nrm pl))
+   else let dist =  (((pnt pl -. pos ry) `dot` nrm pl) / (look ry `dot` nrm pl)) in 
+      Just (raydist2distpoint dist ry)
 
 --reflects the direction of a given vector off a given plane
 reflectDirOff :: Plane -> Vec -> Vec 
@@ -128,7 +129,10 @@ reflectDirOff pl dir =
 
 data Sphere = Sphere {radius :: Double, center :: Vec}
 
-sphereIntersectDist :: Sphere -> Ray -> Maybe Double
+
+raydist2distpoint ry dst = (dist, look ry *. dist +. pos ry)
+
+sphereIntersectDist :: Sphere -> Ray -> Maybe (Double,Vec)
 sphereIntersectDist sph ry = 
    let r2 = radius sph * radius sph in 
    let center2pos = center sph -. pos ry in 
@@ -141,14 +145,47 @@ sphereIntersectDist sph ry =
       let ans1 = -loc - back2frontDist in 
       --ans1 is the distance to the front of the sphere and ans2 the distance to the back of the sphere
       --if you're not inside the sphere, you want ans1
-      if ans1 > 0 then Just ans1 else 
+      if ans1 > 0 then Just (raydist2distpoint ans1 ry) else 
          let ans2 = -loc + back2frontDist in 
-         Just ans2
+         Just (raydist2distpoint ans2 ry)
+
+transformSpherePointToPlane :: Sphere -> Vec -> Plane
+transformSpherePointToPlane sph pnt = 
+   let ctr = center sph in                -- center of the sphere
+   let nrm = normalize (pnt -. ctr) in    -- normal is the vector from the center of the sphere to the intersection point, normalized
+   Plane pnt nrm
 
 data RayResult = NoHit |
-   Hit {dist :: Float, color :: Vec}
+   Hit {dist :: Float, color :: Vec, newRay :: Maybe Ray}
 
 type Traceable = Ray -> RayResult
+--TODO: ADD OBJECT IN THE SCENE HERE
+-- object 1: a reflective sphere
+trcbl1 :: Traceable
+trcbl1 ry = 
+   let sph = Sphere rad (centerx,centery,centerz)) in       -- declare the sphere
+   let (dist,pnt) = sphereIntersectDist sph ry in          -- calculate distance and hitPoint
+   if dist then                                             -- Ray hit the sphere
+      let pl = transformSpherePointToPlane sph pnt in             -- get plane generated from the hit point on sphere
+      let newRay = Ray pnt (reflectDirOff pl (look ry)) in        -- get the reflected ray
+      let hitResult = Hit dist (colorr,colorg,colorb) newRay in   -- return the hitresult with distance, color and new ray
+      Just hitResult
+   else Just NoHit                                          -- Ray miss the sphere, return NoHit
+
+-- object 2: a non-reflective plane
+trcbl2 :: Traceable
+trcbl2 ry = 
+   let pl = Plane (pntx,pnty,pntz) (nrmx,nrmy,nrmz) in   -- declare the plane
+   let (dist,pnt) = planeIntersectDist pl ry in                -- calculate distance and hitpoint
+   if dist then                                          -- Ray hit the sphere
+      let newRay = Nothing in                                     -- set to Nothing for non-reflective item
+      let hitResult = Hit dist (colorr,colorg,colorb) newRay in   -- return the hitresult with distance, color and Nothing
+      Just hitResult
+   else Just NoHit                                       -- Ray miss the sphere, return NoHit
+
+--Add all traceable to a list so that can be used later
+globalTraceablelist :: [Traceable]
+globalTraceablelist = (trcbl1,trcbl2)
 
 data Camera = Camera {position :: Vec, fov, yaw, pitch :: Double}
 
@@ -174,3 +211,32 @@ getRayAtPixel cam sidelength x y =
    let lk = camrot *.. (dist,xf2,yf2) in 
       Ray {pos = position cam, look = lk}
    
+   getHitTraceableOfRay :: Ray -> [Traceable] -> Maybe Traceable
+   getHitTraceableRay _ [] = Nothing
+   getHitTraceableOfRay ry (trcbl:trcbls) =
+      let hitResult1 = trcbl ry
+      let trcbl2 = getHitTraceableOfRay ry trcbls
+      if (hitresult1 == NoHit) && (trcbl2 == Nothing)    -- if current traceble does not hit and there are no hit either in the rest of the list
+         then Just Nothing else                             --return Nothing
+      if (hitresult1 == NoHit)                           -- if current traceble does not hit but there are hit in the rest of the list
+         then Just trcbl2 else                              --return result from the rest of the list
+      let dist1 = dist (hitResult1) in                   -- if both current traceble hit and there are hit in the rest of the list 
+      let trcbl2 = getHitTraceableOfRay ry trcbls
+      let hitResult2 = trcbl2 ry
+      let dist2 = dist (hitResult2) in                               -- compare the distance of current object 
+      if (dist1 <= dist 2)                                           -- to the closest object in the rest of the list
+         then Just trcbl else Just trcbl2                            -- return the one has closer distance
+      
+   getColorOfRay :: Ray -> Int -> Double -> Vec
+   getColorOfRay ry limit totalDist =
+      if length(globalTraceablelist) == 0                         -- if list is empty 
+         then Just (0,0,0) else                                      -- return BLACK
+      let trcbl = (getHitTraceableOfRay ry (globalTraceablelist)) in 
+      if trcbl == Nothing                                         -- if this ray does not hit any object
+         then Just (0,0,0) else                                      -- return BLACK
+      let hitResult = trcbl ry in                           
+      let dimCoef = 0.999 * (totalDist + dist (hitResult)) in                                        
+      if (newRay (hitResult) == Nothing) || (limit == 0)          -- if it hit, and newRay is nothing or the bounce limit is reached
+         then Just (map (dimCoef *) (color hitResult)) else          -- then return the color of current object (*modified by dimCoef)
+      let newRayColor = (getColorOfRay (newRay (hitResult)) (limit - 1) (totalDist + dist (hitResult)) in -- get color of the newRay, if it hit, and newRay is not nothing 
+      Just (map (dimCoef *) (color hitResult)) +. newRayColor        --then mix current color with the color of the newRay (*modified by dimCoef)
